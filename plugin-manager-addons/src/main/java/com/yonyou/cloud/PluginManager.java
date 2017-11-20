bf690b730b086f9bb3053322c0ecdc6edbd71bc0
zhukai1 <zhukai1@yonyou.com>
2017-11-20 11:42:40 +0800

    更新日志显示

--- plugin-manager-addons/src/main/java/com/yonyou/cloud/PluginManager.java
+++ plugin-manager-addons/src/main/java/com/yonyou/cloud/PluginManager.java
@@ -13,12 +13,21 @@ import org.slf4j.LoggerFactory;
 import com.yonyou.cloud.itf.PluginExtensionItf;
 
 public class PluginManager {
-	private static final Logger logger = LoggerFactory.getLogger(PluginManager.class);
+	private static Logger logger;
+	private static boolean logflag =true;
 	private static PluginExtensionItf[] plugins;
 
 
 	static {
-		logger.info("初始化微服务插件管理器PluginManager");
+		try {
+			logger = LoggerFactory.getLogger(PluginManager.class);
+		} catch (Throwable e) {
+			logger =null;
+			logflag=false;
+		}
+		if(logflag) {
+			logger.info("初始化微服务插件管理器PluginManager");
+		}
 		ServiceLoader<PluginExtensionItf> serviceLoader = ServiceLoader.load(PluginExtensionItf.class);
 		Iterator<PluginExtensionItf> it = serviceLoader.iterator();
 		List<PluginExtensionItf> list = new ArrayList<PluginExtensionItf>();
@@ -26,7 +35,9 @@ public class PluginManager {
 
 			PluginExtensionItf itf = it.next();
 			list.add(itf);
-			logger.info("加载插件" + itf.getPluginName());
+			if(logflag) {
+				logger.info("加载插件" + itf.getPluginName());
+			}
 		}
 		Collections.sort(list, new Comparator<PluginExtensionItf>() {
 
@@ -41,12 +52,16 @@ public class PluginManager {
 			}
 
 		});
-		plugins = list.toArray(plugins);
-		logger.info("初始化微服务插件管理器PluginManager 成功 已加载插件" + plugins.length + "个");
+		plugins = list.toArray(new PluginExtensionItf[1]);
+		if(logflag) {
+			logger.info("初始化微服务插件管理器PluginManager 成功 已加载插件" + plugins.length + "个");
+		}
 	}
 
 	public static void runPlugins(ExtensionContext context) {
-		logger.info("执行插件链条");
+		if(logflag) {
+			logger.info("执行插件链条");
+		} 
 	    PluginInvokerChain chain = new PluginInvokerChain();
 		for (int i=0;i<plugins.length;i++) {
 			chain.run(plugins, context);
--- plugin-manager-dependency/src/main/java/com/yonyou/cloud/ExtensionContext.java
+++ plugin-manager-dependency/src/main/java/com/yonyou/cloud/ExtensionContext.java
@@ -1,7 +1,13 @@
 package com.yonyou.cloud;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class ExtensionContext {
 	public Map<String,Object> ContextMap;
+	
+	public ExtensionContext() {
+		 ContextMap= new HashMap<String, Object>();
+	}
+	
 }
